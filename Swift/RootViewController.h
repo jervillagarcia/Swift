dce05afc338071e2557eb21efdd05cfe0f611a8f
Errol <errol@Apply-Financial-iMac.local>
2011-08-16 12:45:20 +0800

    Initial Commit

---
+++ Swift/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Swift
+//
+//  Created by Errol on 8/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ SwiftTests/SwiftTests.m
@@ -0,0 +1,33 @@
+//
+//  SwiftTests.m
+//  SwiftTests
+//
+//  Created by Errol on 8/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SwiftTests.h"
+
+
+@implementation SwiftTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SwiftTests");
+}
+
+@end
