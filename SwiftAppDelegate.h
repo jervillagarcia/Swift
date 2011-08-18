bb2605bbf98dbcc55b35ce13409e37c79bde8e7b
Errol <errol@Apply-Financial-iMac.local>
2011-08-18 13:14:06 +0800

    08-18-2011: Added SwiftAppDelegate

---
+++ SwiftAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAppDelegate.h
+//  Swift
+//
+//  Created by Errol on 8/16/11.
+//  Copyright 2011 ApplyFinancial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwiftAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
