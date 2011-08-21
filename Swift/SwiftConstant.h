fa20da037334419ad9f7ff89745793d40a0fc244
Errol Villagarcia <errol.villagarcia@applyfinancial.co.uk>
2011-08-22 00:19:02 +0800

    Added: ** ErrorCodes List and Constants

---
+++ Swift/SwiftConstant.h
@@ -0,0 +1,44 @@
+//
+//  SwiftConstant.h
+//  Swift
+//
+//  Created by Errol on 8/21/11.
+//  Copyright 2011 ApplyFinancial. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SwiftConstant : NSObject {
+    
+}
+
+extern NSString* const kErrorCodeER00001;
+extern NSString* const kErrorCodeER00002;
+extern NSString* const kErrorCodeER00003;
+extern NSString* const kErrorCodeER00004;
+extern NSString* const kErrorCodeER00005;
+extern NSString* const kErrorCodeER00006;
+extern NSString* const kErrorCodeER00007;
+extern NSString* const kErrorCodeER00008;
+extern NSString* const kErrorCodeER00009;
+extern NSString* const kErrorCodeER00010;
+extern NSString* const kErrorCodeER00011;
+extern NSString* const kErrorCodeER00012;
+extern NSString* const kErrorCodeER00013;
+extern NSString* const kErrorCodeER00014;
+extern NSString* const kErrorCodeER00015;
+extern NSString* const kErrorCodeER00016;
+extern NSString* const kErrorCodeER00017;
+extern NSString* const kErrorCodeER00018;
+extern NSString* const kErrorCodeER00019;
+extern NSString* const kErrorCodeER00020;
+extern NSString* const kErrorCodeER00021;
+extern NSString* const kErrorCodeER00022;
+extern NSString* const kErrorCodeER00023;
+extern NSString* const kErrorCodeER00026;
+extern NSString* const kErrorCodeER00027;
+extern NSString* const kErrorCodeER00028;
+extern NSString* const kErrorCodeER00029;
+
+@end
---
+++ Swift/SwiftConstant.m
@@ -0,0 +1,42 @@
+//
+//  SwiftConstant.m
+//  Swift
+//
+//  Created by Errol on 8/21/11.
+//  Copyright 2011 ApplyFinancial. All rights reserved.
+//
+
+#import "SwiftConstant.h"
+
+
+@implementation SwiftConstant
+
+NSString* const kErrorCodeER00001 = @"ER00001";
+NSString* const kErrorCodeER00002 = @"ER00002";
+NSString* const kErrorCodeER00003 = @"ER00003";
+NSString* const kErrorCodeER00004 = @"ER00004";
+NSString* const kErrorCodeER00005 = @"ER00005";
+NSString* const kErrorCodeER00006 = @"ER00006";
+NSString* const kErrorCodeER00007 = @"ER00007";
+NSString* const kErrorCodeER00008 = @"ER00008";
+NSString* const kErrorCodeER00009 = @"ER00009";
+NSString* const kErrorCodeER00010 = @"ER00010";
+NSString* const kErrorCodeER00011 = @"ER00011";
+NSString* const kErrorCodeER00012 = @"ER00012";
+NSString* const kErrorCodeER00013 = @"ER00013";
+NSString* const kErrorCodeER00014 = @"ER00014";
+NSString* const kErrorCodeER00015 = @"ER00015";
+NSString* const kErrorCodeER00016 = @"ER00016";
+NSString* const kErrorCodeER00017 = @"ER00017";
+NSString* const kErrorCodeER00018 = @"ER00018";
+NSString* const kErrorCodeER00019 = @"ER00019";
+NSString* const kErrorCodeER00020 = @"ER00020";
+NSString* const kErrorCodeER00021 = @"ER00021";
+NSString* const kErrorCodeER00022 = @"ER00022";
+NSString* const kErrorCodeER00023 = @"ER00023";
+NSString* const kErrorCodeER00026 = @"ER00026";
+NSString* const kErrorCodeER00027 = @"ER00027";
+NSString* const kErrorCodeER00028 = @"ER00028";
+NSString* const kErrorCodeER00029 = @"ER00029";
+
+@end
