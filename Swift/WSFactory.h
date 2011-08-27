5a15606857a27c97f61480afa5a554ce4b5cc242
Errol Villagarcia <errol.villagarcia@applyfinancial.co.uk>
2011-08-27 10:37:00 +0800

    WS Factory - Initial Commit

---
+++ Swift/WSFactory.h
@@ -0,0 +1,16 @@
+//
+//  WSFactory.h
+//  Swift
+//
+//  Created by James Errol Villagarcia on 8/25/11.
+//  Copyright 2011 ApplyFinancial. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface WSFactory : NSObject {
+    
+}
+
+@end
---
+++ Swift/WSFactory.m
@@ -0,0 +1,14 @@
+//
+//  WSFactory.m
+//  Swift
+//
+//  Created by James Errol Villagarcia on 8/25/11.
+//  Copyright 2011 ApplyFinancial. All rights reserved.
+//
+
+#import "WSFactory.h"
+
+
+@implementation WSFactory
+
+@end
